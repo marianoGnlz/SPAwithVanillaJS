2e25a6344b6fbd3cde5eeee6a1e00b7f3cde6f36
marianoGnlz <marianoGnlz@users.noreply.github.com>
2020-08-19 16:35:41 -0300

    Unico commit

---
+++ js/routes.js
@@ -0,0 +1,14 @@
+const PATHS = {
+    home: {
+        path: "/",
+        template: `<h1>🏠 Home</h1>`,
+    },
+    about: {
+        path: "/about",
+        template: `<h1>👩🏻‍💻 Sobre mi</h1>`,
+    },
+    contact: {
+        path: "/contact",
+        template: `<h1>📱 Contacto</h1>`,
+    }
+}
